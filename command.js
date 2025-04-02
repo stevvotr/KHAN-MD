1efab88cc219f44c748e60e449d3f5173cb3fc60
stevvotr <nicosamto02@gmail.com>
2025-04-02 12:30:46 +0100

    Update command.js

--- command.js
+++ command.js
@@ -3,17 +3,17 @@ var commands = [];
 function cmd(info, func) {
     var data = info;
     data.function = func;
-    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!data.dontAddCommandList) data.KHAN-MD~W64ThLSS#tfeb_JHdbQ17gUUmq3MNkOljoiKdxz7fMg_sF7OV1cY = false;
     if (!info.desc) info.desc = '';
     if (!data.fromMe) data.fromMe = false;
     if (!info.category) data.category = 'misc';
-    if(!info.filename) data.filename = "Not Provided";
+    if(!info.filename) data.Stevo west  = "Not Provided";
     commands.push(data);
     return data;
 }
 module.exports = {
     cmd,
-    AddCommand:cmd,
+    AddCommand:cmd, 
     Function:cmd,
     Module:cmd,
     commands,
